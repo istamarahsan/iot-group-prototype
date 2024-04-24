9fea228b14c2cb7a7114041cd8e300055d8c3159
istamarahsan <ahsan.istamar@proton.me>
2024-04-24 08:55:22 +0300

    access key for classifier

--- classifier/main.py
+++ classifier/main.py
@@ -1,16 +1,23 @@
 from transformers import pipeline
 from fastapi import FastAPI, Request
+import os
+import http
 
 classifier_pipe = pipeline("audio-classification", model="MIT/ast-finetuned-audioset-10-10-0.4593")
 
 app = FastAPI()
 
+TOKEN = os.environ["ACCESS_KEY"]
+
 @app.get("/health")
-async def healthcheck():
-    return 200
+async def healthcheck(request: Request):
+    return http.HTTPStatus.OK if TOKEN != "" and request.headers['Authorization'] == f"Bearer {TOKEN}" else http.HTTPStatus.UNAUTHORIZED
 
 @app.post("/classify")
 async def classify(request: Request):
+    if TOKEN != "" and request.headers['Authorization'] == f"Bearer {TOKEN}":
+        return http.HTTPStatus.UNAUTHORIZED
+
     bytes = await request.body()
     result = classifier_pipe(bytes)
     return result
